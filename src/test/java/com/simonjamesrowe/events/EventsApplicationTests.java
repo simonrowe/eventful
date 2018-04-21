34150f8de6baf01ab789e66ae0bfae0b83591e60
Simon Rowe <simon.rowe@umusic.com>
2018-04-21 20:20:33 +0100

    Initial Commit - New Sprint boot project created from spring initializer - start.spring.io

---
+++ src/test/java/com/simonjamesrowe/events/EventsApplicationTests.java
@@ -0,0 +1,21 @@
+package com.simonjamesrowe.events;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class EventsApplicationTests {
+
+	@Autowired
+	private TestRestTemplate testRestTemplate;
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
