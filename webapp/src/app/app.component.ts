38300bf69ef339a317c0071be3ca21e0c758ca6d
Simon Rowe <simon.rowe@gmail.com>
2018-04-23 10:24:41 +0100

    more elastic search info

--- webapp/src/app/app.component.ts
+++ webapp/src/app/app.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {environment} from '../environments/environment';
+import {DatePipe} from '@angular/common';
+import {FormBuilder, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
@@ -11,19 +13,69 @@ export class AppComponent implements OnInit{
 
 
   searchResults: any;
+  q: string;
+  sortField: string;
+  sortOrder: string;
+  page: number;
+  size: number = 10 ;
+  searchForm: FormGroup;
 
-  constructor(private http: HttpClient) {
+
+  constructor(private http: HttpClient, private fb: FormBuilder) {
+    this.searchForm = fb.group({});
   }
 
   ngOnInit(): void {
-    this.http.get(environment.eventRestEndpoint).subscribe((res: any) => {
-      this.searchResults = res;
-    });
+    this.clear();
   }
 
   public hasResults(): boolean {
     return this.searchResults != null && this.searchResults.content != null && this.searchResults.content.length > 0;
   }
+
+  public search(): void {
+    this.resetState();
+    this.executeSearch();
+  }
+
+  private executeSearch(): void {
+    let params: string = '';
+    params += 'page=' + this.page + '&size=' + this.size;
+    if (this.sortField != null && this.sortField != '') {
+      params += '&sort=' + this.sortField + "," + this.sortOrder
+    }
+    if (this.q != null && this.q != '') {
+      params += '&q=' + this.q
+    }
+    this.http.get(environment.eventRestEndpoint + '?' + params).subscribe((res: any) => {
+      this.searchResults = res;
+    });
+  }
+
+  private resetState(): void {
+    this.page = 1;
+    this.sortField = 'startTime';
+    this.sortOrder = 'asc';
+  }
+
+  public clear(): void {
+    this.resetState();
+    this.q = '';
+    this.executeSearch();
+  }
+
+  public sort(column: string): void {
+    if (column != this.sortField) {
+       this.resetState();
+       this.sortField = column;
+       this.executeSearch();
+    } else {
+      this.page = 1;
+      this.sortOrder= this.sortOrder == 'asc' ? 'desc' : 'asc';
+      this.executeSearch();
+    }
+  }
+
 
 
 }
